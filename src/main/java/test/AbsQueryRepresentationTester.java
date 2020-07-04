316dcfe41aba8c3723f3a17e297d760c7eb361b3
pvassil <panos.vassiliadis@gmail.com>
2020-07-04 18:40:44 +0300

    Updated tester of AQR

--- src/main/java/test/AbsQueryRepresentationTester.java
+++ src/main/java/test/AbsQueryRepresentationTester.java
@@ -17,10 +17,11 @@ class AbsQueryRepresentationTester {
 	private static AbstractQueryRepresentation aqr;
 	private static Map<String, Integer> propertiesSet;
 	private static Map<CharacteristicSet, Integer> csMap;
+	private static Map<String, String> prefixMap;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		Map<String, String> prefixMap = new HashMap<String,String>();
+		prefixMap = new HashMap<String,String>();
 		prefixMap.put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 		prefixMap.put("ub", "http://swat.cse.lehigh.edu/onto/univ-bench.owl#");
 
@@ -39,30 +40,46 @@ class AbsQueryRepresentationTester {
 		csMap.put(new CharacteristicSet(Arrays.asList(0, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12)), 5 );
 		csMap.put(new CharacteristicSet(Arrays.asList(0, 2, 16)), 12); 
 		
-		/*
-		public static String q1_ext = prefix + 
-				"SELECT ?X ?Y WHERE"
-				+ "{?X rdf:type ub:GraduateStudent . "
-				+ "?X ub:takesCourse ?Y . "
-				+ "?Y rdf:type ?gr}"; 
-		*/
-		
+		propertiesSet = new HashMap<String, Integer>();
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#advisor>" , 15);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#undergraduateDegreeFrom>" , 5);
+		propertiesSet.put("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>" , 0);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#teacherOf>" , 4);
+		propertiesSet.put("<http://www.w3.org/2002/07/owl#imports>" , 1);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#name>" , 2);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#telephone>" , 10);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#emailAddress>" , 9);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#publicationAuthor>" , 16);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#takesCourse>" , 14);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#teachingAssistantOf>" , 17);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#doctoralDegreeFrom>" , 7);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#subOrganizationOf>" , 3);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#researchInterest>" , 11);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#mastersDegreeFrom>" , 6);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#memberOf>" , 13);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#worksFor>" , 8);
+		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#headOf>" , 12);
 		/*
 		 * TODO: test also with sth that has ?W: * !!!!
 		 */
-		
+	}
+
+	
+	/*
+	public static String q1_ext = prefix + 
+			"SELECT ?X ?Y WHERE"
+			+ "{?X rdf:type ub:GraduateStudent . "
+			+ "?X ub:takesCourse ?Y . "
+			+ "?Y rdf:type ?gr}"; 
+	*/
+
+	@Test
+	void testConvertToPropertyIds() {	
 		Map<String, List<String>> queryDependencies = new HashMap<String, List<String>>();
 		queryDependencies.put("?X",Arrays.asList("rdf:type", "ub:takesCourse"));
 		queryDependencies.put("?Y",Arrays.asList("rdf:type"));
 
 		aqr = new AbstractQueryRepresentation(queryDependencies, prefixMap);
-		propertiesSet = new HashMap<String, Integer>();
-		propertiesSet.put("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>", 0);
-		propertiesSet.put("<http://swat.cse.lehigh.edu/onto/univ-bench.owl#takesCourse>", 1);
-	}
-
-	@Test
-	void testConvertToPropertyIds() {
 
 		int conVersionSize = aqr.convertToPropertyIds(propertiesSet);
 		assertEquals(2,conVersionSize);
@@ -78,6 +95,12 @@ class AbsQueryRepresentationTester {
 	
 	@Test
 	void testComputeAllNeededStuffForAQR() {
+		Map<String, List<String>> queryDependencies = new HashMap<String, List<String>>();
+		queryDependencies.put("?X",Arrays.asList("rdf:type", "ub:takesCourse"));
+		queryDependencies.put("?Y",Arrays.asList("rdf:type"));
+
+		aqr = new AbstractQueryRepresentation(queryDependencies, prefixMap);
+
 		aqr.computeAllNeededStuffForAQR(propertiesSet,csMap);
 		for(String s: aqr.getVariableDependencies().keySet()) {
 			System.out.print(s + " ->\t" );
