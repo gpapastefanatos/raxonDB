b6b547a030a244f828c334bc2e3c62f3bf98c898
gpapastefanatos <gpapastefanatos@gmail.com>
2020-07-09 13:45:56 +0300

    Rename classes

---
+++ src/main/java/com/athena/imis/schema/managment/DensityFactorOptimizerClientIS20.java
@@ -0,0 +1,14 @@
+package com.athena.imis.schema.managment;
+
+
+
+public class DensityFactorOptimizerClientIS20 {
+
+	public static void main(String[] args) {
+		int initialDensityFactor = 0;
+		DensityFactorOptimizerIS20 schemaDecisionEngine = new DensityFactorOptimizerIS20(initialDensityFactor, args);
+		int result = schemaDecisionEngine.optimizeDensityFactor();
+
+		System.out.println("Density Factor returned " + result);
+	}
+}
